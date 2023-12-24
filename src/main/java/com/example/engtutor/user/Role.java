ec914f65670ad0fd526eba8b0903d8ffe8acdfaa
Hnidyi Dmytro <dmytroprog@gmail.com>
2023-12-24 20:29:44 +0200

    Added user model, repository and Role

---
+++ src/main/java/com/example/engtutor/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.engtutor.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/engtutor/user/User.java
@@ -0,0 +1,67 @@
+package com.example.engtutor.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="_user")
+public class User implements UserDetails{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String login;
+    private String password;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
